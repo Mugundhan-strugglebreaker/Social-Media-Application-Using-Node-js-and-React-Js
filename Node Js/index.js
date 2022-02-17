d420fd2f446f2bba98782843918f1581b22fdad5
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 14:38:28 +0530

    Establish MonogoDb Connection

--- Node Js/index.js
+++ Node Js/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const dotenv =  require("dotenv")
+const morgan = require("morgan")
+const helmet = require("helmet")
 
+dotenv.config()
 
-app.listen(8000,()=>{
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser : true},()=>{
+    console.log("Connected to monogoDb");
+});
+
+app.listen(8800,()=>{
     console.log("Back end service running");
 })

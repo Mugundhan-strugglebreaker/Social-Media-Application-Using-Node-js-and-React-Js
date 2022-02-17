4ac032625220ccf50ff668bbbece5490c3fb0a23
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 19:20:45 +0530

    Unfollow a User

--- Node Js/routes/users.js
+++ Node Js/routes/users.js
@@ -84,6 +84,33 @@ router.put("/follow/:id", async (req,res)=>{
 })
 
 //unfollow a user
+router.put("/unfollow/:id", async (req,res)=>{
+   
+    if(req.body.userId !== req.params.id){
+        
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            
+            if(user.followers.includes(req.body.userId)){
+                
+                await user.updateOne({ $pull : {followers :req.body.userId}});
+                await currentUser.updateOne({ $pull : {followings : req.params.id}});
+                res.status(200).json("User has been Unfollowed")
+
+
+            }else{
+                res.status(403).json("You dont follow this user");
+            }
+
+        }catch(err){
+            res.status(500).json(err)
+        }
+
+    }else{
+        res.status(403).json("You cannot unfollow yourself");
+    }
+})
 
 
 module.exports = router

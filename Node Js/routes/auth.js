a785c43ca02e4f21ac50ddd411b0753f099930c3
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 17:23:13 +0530

    Login api

--- Node Js/routes/auth.js
+++ Node Js/routes/auth.js
@@ -23,7 +23,26 @@ router.post("/register",async (req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user);
     }catch(err){
-        console.log(err)
+        res.status(500).json(err)
+    }
+
+})
+
+//Login
+router.post("/login", async (req,res)=>{
+
+    try{
+
+        const user = await User.findOne({email : req.body.email});
+        !user && res.status(404).json("User Not Found!!!");
+
+        const validPassword = await bcrypt.compare(req.body.password , user.password)
+        !validPassword && res.status(400).json("Wrong Password");
+
+        res.status(200).json(user);
+
+    }catch(err){
+        res.status(500).json(err)
     }
 
 })

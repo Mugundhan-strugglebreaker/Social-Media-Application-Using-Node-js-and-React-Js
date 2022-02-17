3b131d0d137fdb047b1bf3bdab531f182dfe4a0b
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 19:34:42 +0530

    Created Post Model

--- Node Js/index.js
+++ Node Js/index.js
@@ -6,20 +6,9 @@ const morgan = require("morgan")
 const helmet = require("helmet")
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 
 dotenv.config()
-
-// mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser : true , useUnifiedTopology:true},()=>{
-//     console.log("Connected to monogoDb");
-// });
-
-// mongoose.connect(
-//     process.env.MONGO_URL,
-//     { useNewUrlParser: true, useUnifiedTopology: true},
-//     () => {
-//       console.log('Connected to MongoDB');
-//     }
-//   );
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -35,6 +24,7 @@ app.use(helmet())
 
 app.use("/api/users",userRoute)
 app.use("/api/auth",authRoute)
+app.use("/api/posts",postRoute)
 
 app.listen(8800,()=>{
     console.log("Back end service running");

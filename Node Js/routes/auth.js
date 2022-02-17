4d0f6f2f1c4c84d12ce7099c31a45f4efe5bc4b2
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 16:54:32 +0530

    Adding Packages for Hasing and Generate hashed password

--- Node Js/routes/auth.js
+++ Node Js/routes/auth.js
@@ -1,15 +1,25 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
+
 
 //Register
 router.post("/register",async (req,res)=>{
 
-    const newUser = new User({
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password
-    });
+    
     try{
+        //Generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password , salt)
+
+        //Create User
+        const newUser = new User({
+            username : req.body.username,
+            email : req.body.email,
+            password : hashedPassword
+        });
+
+        //Save User and Respond
         const user = await newUser.save();
         res.status(200).json(user);
     }catch(err){

50bc809ddb865e09cf0b8070c4d3b42e224eb0ed
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-17 15:18:10 +0530

    Creating User Model and Route and Auth Route

--- Node Js/index.js
+++ Node Js/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const dotenv =  require("dotenv")
 const morgan = require("morgan")
 const helmet = require("helmet")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
@@ -14,9 +16,11 @@ mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser : true},()=>{
 
 //middleware
 app.use(express.json())
-app.use(morgan())
+app.use(morgan("common"))
 app.use(helmet())
 
+app.use("/api/users",userRoute)
+app.use("/api/auth",authRoute)
 
 app.listen(8800,()=>{
     console.log("Back end service running");
---
+++ Node Js/routes/users.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+
+router.get("/",(req,res)=>{
+    res.send("Welcome to user route");
+})
+
+module.exports = router
+
+

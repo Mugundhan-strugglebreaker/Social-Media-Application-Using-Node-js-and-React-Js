8369dcf77ffd1347e5a8909d22c61985faf4dd60
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-18 12:57:52 +0530

    Like and DisLike Post

--- Node Js/routes/posts.js
+++ Node Js/routes/posts.js
@@ -41,5 +41,21 @@ router.delete("/:id",async (req,res)=>{
         res.status(500).json(err)
     }
 })
+//Like or Dislike a Post
+router.put("/like/:id", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push : { likes : req.body.userId}})
+            res.status(200).json("The Post has been liked");
+        }else{
+            await post.updateOne({$pull : { likes : req.body.userId}})
+            res.status(200).json("The Post has been disliked");
+        }
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router

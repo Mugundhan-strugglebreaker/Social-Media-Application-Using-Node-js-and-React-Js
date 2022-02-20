22b36d62f76b0fd4f52e96949c0ab9c7e060325f
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-20 20:17:38 +0530

    Adding Dynamic Data for Post Using dummyData.js file

--- react_js/src/components/post/Post.jsx
+++ react_js/src/components/post/Post.jsx
@@ -1,32 +1,32 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
-
-function Post() {
+import { Users } from "../../dummyData"
+function Post({post}) {
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assests/person/1.jpg" alt="" className="postProfileImg" />
-                    <span className="postUsername">Mugundhan</span>
-                    <span className="postDate">5 mins ago</span>
+                    <img src={Users.filter((u)=> u.id===post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                    <span className="postUsername">{Users.filter((u)=> u.id===post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! It's my first post :)</span>
-                <img src="/assests/post/1.jpg" alt="" className="postImg" />
+                <span className="postText">{post?.desc}</span>
+                <img src={post.photo} alt="" className="postImg" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                         <img className="likeIcon" src="/assests/like.png" alt="" />
                         <img className="heartIcon" src="/assests/heart.png" alt=""/>
-                        <span className="postLikeCounter">32 people like it</span>
+                        <span className="postLikeCounter">{post.like} people like it</span>
                 </div>
                 <div className="postBottomRight">
-                     <span className="postCommentText">9 Comments</span>
+                     <span className="postCommentText">{post.comment} Comments</span>
                 </div>
             </div>
         </div>
---
+++ react_js/src/dummyData.js
@@ -0,0 +1,94 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture:"assests/person/1.jpg",
+        username:"Mugundhan S"
+
+    },{
+        id:2,
+        profilePicture:"assests/person/2.jpg",
+        username:"Meiyappan N"
+    },{
+        id:3,
+        profilePicture:"assests/person/3.jpg",
+        username:"Mukesh"
+    },{
+        id:4,
+        profilePicture:"assests/person/4.jpg",
+        username:"Rajesh"
+
+    },{
+        id:5,
+        profilePicture:"assests/person/5.jpg",
+        username:"Vignesh"
+
+    },{
+        id:6,
+        profilePicture:"assests/person/6.jpg",
+        username:"Arnold"
+
+    },{
+        id:7,
+        profilePicture:"assests/person/7.jpg",
+        username:"Aravind"
+
+    },{
+        id:8,
+        profilePicture:"assests/person/8.jpg",
+        username:"Ronaldo"
+
+    },{
+        id:9,
+        profilePicture:"assests/person/9.jpg",
+        username:"Messi"
+
+    },{
+        id:10,
+        profilePicture:"assests/person/10.jpg",
+        username:"Ben 10"
+
+    }
+]
+export const Posts = [
+    {
+        id:1,
+        desc :"Love for all Hatred for None!!!..",
+        date:"5 min ago",
+        photo:"assests/post/1.jpg",
+        userId:1,
+        like:32,
+        comment:9,
+    },
+    {
+        id:2,
+        date:"15 min ago",
+        photo:"assests/post/2.jpg",
+        userId:2,
+        like:2,
+        comment:1,
+    },
+    {
+        id:3,
+        date:"20 min ago",
+        photo:"assests/post/3.jpg",
+        userId:3,
+        like:8,
+        comment:9,
+    },
+    {
+        id:4,
+        date:"55 min ago",
+        photo:"assests/post/4.jpg",
+        userId:4,
+        like:32,
+        comment:9,
+    },
+    {
+        id:5,
+        date:"59 min ago",
+        photo:"assests/post/5.jpg",
+        userId:5,
+        like:32,
+        comment:9,
+    }
+]

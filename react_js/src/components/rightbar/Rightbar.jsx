286e88d999f1e27f4e302c1da87fadc1e83dd711
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-20 19:01:15 +0530

    Creating Rightbar

--- react_js/src/components/rightbar/Rightbar.jsx
+++ react_js/src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,77 @@
 import "./rightbar.css"
+
 function Rightbar() {
   return (
     <div className="rightbar">
-      Rightbar
+      <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="/assests/gift.png" alt="" className="birthdayImg" />
+            <span className="birthdayText">
+              {" "}
+              <b>Meiyappan</b> and <b>2 other friends</b> have a birthday today
+            </span>
+          </div>
+          <img src="/assests/ad.png" alt="" className="rightbarAd" />
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/2.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Meiyappan</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Rajesh</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/4.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Vignesh</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/5.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Aravind</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/6.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Arnold</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/7.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Mukesh</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/8.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ben 10</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="/assests/person/9.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ronaldo</span>
+            </li>
+          </ul>
+      </div>
     </div>
   )
 }

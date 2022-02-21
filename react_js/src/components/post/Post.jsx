9448406051a249776b55c948e732ea17e448816b
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-21 11:56:18 +0530

    Like functionality using useState Hook

--- react_js/src/components/post/Post.jsx
+++ react_js/src/components/post/Post.jsx
@@ -1,7 +1,14 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
 import { Users } from "../../dummyData"
+import { useState } from "react"
 function Post({post}) {
+  const [like,setLike] = useState(post.like)
+  const [isLiked , setIsLiked] = useState(false)
+  const likeHandler = ()=>{
+      setLike (isLiked ? like-1 : like+1)
+      setIsLiked(!isLiked)
+  }  
   return (
     <div className="post">
         <div className="postWrapper">
@@ -21,9 +28,9 @@ function Post({post}) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                        <img className="likeIcon" src="/assests/like.png" alt="" />
-                        <img className="heartIcon" src="/assests/heart.png" alt=""/>
-                        <span className="postLikeCounter">{post.like} people like it</span>
+                        <img className="likeIcon" src="/assests/like.png" onClick={likeHandler} alt="" />
+                        <img className="heartIcon" src="/assests/heart.png" onClick={likeHandler} alt=""/>
+                        <span className="postLikeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight">
                      <span className="postCommentText">{post.comment} Comments</span>

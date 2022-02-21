c3d0662cd1b5e0407af5106d570ea6b658147633
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-21 23:01:45 +0530

    Fetching User Info and Profile pic and Cover pic.

--- react_js/src/components/rightbar/Rightbar.jsx
+++ react_js/src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import "./rightbar.css"
 import {Users} from "../../dummyData";
 import Online from "../online/Online";
 
-function Rightbar({profile}) {
+function Rightbar({user}) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const HomeRightbar = ()=>{
     return(
@@ -33,15 +33,15 @@ function Rightbar({profile}) {
         <div className="rightbarInfo">
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">Salem</span>
+            <span className="rightbarInfoValue">{user.city}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Tamil Nadu</span>
+            <span className="rightbarInfoValue">{user.from}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
+            <span className="rightbarInfoValue">{user.relationship  ===1 ? "Single" : user.relationship ===2 ? "Married" : "-"}</span>
           </div>
         </div>
         <h4 className="rightbarTitle">User Friends</h4>
@@ -77,7 +77,7 @@ function Rightbar({profile}) {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        {profile ? <ProfileRightbar/> : <HomeRightbar/>}
+        {user ? <ProfileRightbar/> : <HomeRightbar/>}
       </div>
     </div>
   )
--- react_js/src/pages/profile/Profile.jsx
+++ react_js/src/pages/profile/Profile.jsx
@@ -3,8 +3,18 @@ import Topbar from "../../components/topbar/Topbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Feed from "../../components/feed/Feed"
 import Rightbar from "../../components/rightbar/Rightbar"
+import { useEffect, useState } from "react"
+import axios from "axios"
 function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user,setUser] = useState({})
+  useEffect(()=>{
+    const fetchUser = async ()=>{
+        const res = await axios.get(`/users?username=MugundhanS`)
+        setUser(res.data)
+    }
+    fetchUser();  
+  },[])
   return (
     <>
       <Topbar/>
@@ -13,18 +23,18 @@ function Profile() {
           <div className="profileRight">
                <div className="profileRightTop">
                     <div className="profileCover">
-                        <img src={`${PF}post/1.jpg`} alt="" className="profileCoverImg" />
-                        <img src={`${PF}person/1.jpg`} alt="" className="profileUserImg" />
+                        <img src={user.coverPicture || PF+"/coverPic.jpg"} alt="" className="profileCoverImg" />
+                        <img src={user.profilePicture || PF+"/profilePic.png"} alt="" className="profileUserImg" />
                     </div>
                     <div className="profileInfo">
-                        <h4 className="profileInfoName">Mugundhan</h4>
-                        <span className="profileInfoDesc">Be a Leader</span>
+                        <h4 className="profileInfoName">{user.username}</h4>
+                        <span className="profileInfoDesc">{user.desc}</span>
                     </div>
                    
                </div>
                <div className="profileRightBottom">
                    <Feed username="MugundhanS" />
-                   <Rightbar profile/>
+                   <Rightbar user={user}/>
                </div>            
           </div>
         </div>

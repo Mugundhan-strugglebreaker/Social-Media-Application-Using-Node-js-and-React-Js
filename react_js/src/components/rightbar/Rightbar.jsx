f3dc0d0a4b8d2c21590e75be325c003277c14f16
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-21 11:28:14 +0530

    Inserting Dynamic Data for Online Friends

--- react_js/src/components/rightbar/Rightbar.jsx
+++ react_js/src/components/rightbar/Rightbar.jsx
@@ -1,6 +1,9 @@
 import "./rightbar.css"
+import {Users} from "../../dummyData";
+import Online from "../online/Online";
 
 function Rightbar() {
+
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
@@ -14,62 +17,11 @@ function Rightbar() {
           <img src="/assests/ad.png" alt="" className="rightbarAd" />
           <h4 className="rightbarTitle">Online Friends</h4>
           <ul className="rightbarFriendList">
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/2.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Meiyappan</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/3.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Rajesh</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/4.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Vignesh</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/5.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Aravind</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/6.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Arnold</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/7.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Mukesh</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/8.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Ben 10</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="/assests/person/9.jpg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Ronaldo</span>
-            </li>
+            {
+              Users.map( (u)=>(
+                <Online key={u.id} user={u}/>
+              ))
+            }
           </ul>
       </div>
     </div>

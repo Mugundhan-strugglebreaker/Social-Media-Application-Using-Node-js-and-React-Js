131e0cc25ff54faaca65686385b453e87edbc00f
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-22 19:52:40 +0530

    Conneting login page and home page

--- react_js/src/App.js
+++ react_js/src/App.js
@@ -5,17 +5,22 @@ import Register from "./pages/register/Register"
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Link,
+  Navigate
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {user} = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/" element={ user ? <Home/> : <Login/>}/>
+        <Route exact path="/login" element={ user ? <Navigate to="/" /> : <Login/>}/>
         <Route exact path="/profile/:username" element={<Profile/>}/>
-        <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/register" element={ user ? <Navigate to="/" /> : <Register/>}/>
       </Routes>
     </Router>
   );

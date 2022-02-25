445e8a3f83d559cdad68c2f146cdb0e755acca1f
Mugundhan-strugglebreaker <mugundsure@gmail.com>
2022-02-25 12:52:26 +0530

    Creating a new message api

--- Node Js/routes/messages.js
+++ Node Js/routes/messages.js
@@ -1,6 +1,17 @@
 const router = require("express").Router()
 const Message = require("../models/Message");
 
+//add a message
+router.post("/", async (req,res)=>{
+    const newMessage = new Message(req.body)
+    try{
+        const savedMessage = await newMessage.save();
+        res.status(200).json(savedMessage);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 
 
 module.exports = router
